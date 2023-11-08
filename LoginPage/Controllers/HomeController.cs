5434ed3e37c815bb82c53ffa8e2a9e6b5e15f788
sirinmehmet <mhmtali45.sirin@gmail.com>
2023-11-08 16:14:37 +0300

    loginpage

---
+++ LoginPage/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Mvc;
+using LoginPage.Models;
+
+
+namespace LoginPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+       loginEntities1 db =new loginEntities1();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(users log)
+        {
+
+            var user =db.users.Where(x => x.Username == log.Username && x.Password == log.Password).Count();
+            if(user>0) 
+            {
+                return RedirectToAction("Dashboard");
+            }
+            else
+            {
+                return View();
+
+            }
+        
+        }
+        public ActionResult Dashboard() 
+        {
+            return View(); 
+        
+        }
+    }
+}
